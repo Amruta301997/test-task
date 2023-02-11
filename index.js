2ab8d7a76a075f64e5bab0aacd6c7c6e38caad22
Amruta Dhawan <amrutadhawan@gmail.com>
2023-02-11 16:04:20 +0530

    complete task

---
+++ index.js
@@ -0,0 +1,11 @@
+
+const { difference } = require('lodash');
+const lodash = require('lodash');
+
+let characters =["a","b","c","d"];
+
+let chunked= lodash.chunk(characters,2);
+
+console.log(chunked);
+
+console.log(lodash.difference([2,1],[2,3]));
